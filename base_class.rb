b8a1fa65ea230eb821f8e22b7573f28613efc5fc
ahaczyk <haczyslaw@gmail.com>
2016-09-29 21:23:26 +0200

    Add validates_* macros concept and validation callbacks - draft

--- base_class.rb
+++ base_class.rb
@@ -1,3 +1,31 @@
 # TASK: Implement this class to make tests pass
 class BaseClass
+
+  def self.validation_callbacks
+    @validation_callbacks ||= Callbacks.new
+  end
+
+  def self.validates_presence_of(argument)
+    callback_name = "_#{argument}_numericality_validation_callback".to_sym
+    validation_callbacks.add(callback_name) do
+      # Add validator here
+    end
+  end
+
+  def self.validates_numericality_of(argument)
+    callback_name = "_#{argument}_numericality_validation_callback".to_sym
+    validation_callbacks.add(callback_name) do
+      # Add validator here
+    end
+  end
+
+  class Callbacks
+    def add(callback_name, &block)
+      define_singleton_method(callback_name, &block)
+    end
+
+    def list
+      @list ||= methods.grep(/_validation_callback/).map { |name| send(name) }
+    end
+  end
 end
