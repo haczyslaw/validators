1bdadefc79268fcd04970d30d83bb06e1c3bc843
ahaczyk <haczyslaw@gmail.com>
2016-09-29 23:49:15 +0200

    Add valid? and errors method

--- base_class.rb
+++ base_class.rb
@@ -1,4 +1,7 @@
 # TASK: Implement this class to make tests pass
+require File.expand_path("../lib/validators/base_validator", __FILE__)
+Dir[("#{__dir__}/lib/validators/*.rb")].each { |f| require f }
+
 class BaseClass
 
   def self.validation_callbacks
@@ -8,14 +11,14 @@ class BaseClass
   def self.validates_presence_of(argument)
     callback_name = "_#{argument}_presence_validation_callback".to_sym
     validation_callbacks.add(callback_name) do
-      # Add validator here
+      Validator::Presence.new(argument)
     end
   end
 
   def self.validates_numericality_of(argument)
     callback_name = "_#{argument}_numericality_validation_callback".to_sym
     validation_callbacks.add(callback_name) do
-      # Add validator here
+      Validator::Numericality.new(argument)
     end
   end
 
@@ -27,5 +30,18 @@ class BaseClass
     def list
       @list ||= methods.grep(/_validation_callback/).map { |name| send(name) }
     end
+  end
+
+  def valid?
+    callbacks = self.class.validation_callbacks.list
+    callbacks.each do |validator|
+      errors << validator.error_message unless validator.instance_is_valid?(self)
+    end
+
+    errors.empty?
+  end
+
+  def errors
+    @errors ||= []
   end
 end
--- lib/validators/presence_validator.rb
+++ lib/validators/presence_validator.rb
@@ -4,7 +4,7 @@ module Validator
 
     def instance_is_valid?(instance)
       variable = instance.send(variable_name)
-      variable.respond_to?(:empty?) ? variable.empty? : variable
+      variable.respond_to?(:empty?) ? !variable.empty? : variable
     end
 
     def tr_message # should be i18n
